30374a220ebeaa631a6a122cbcb70c9cf65417c0
Yining Hu <yining.hu@uts.edu.au>
2024-04-22 15:17:36 +1000

    adds the complete BankTvFx

---
+++ BankTvFx/src/main/java/com/fsd/BankApplication.java
@@ -0,0 +1,17 @@
+package com.fsd;
+
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import com.fsd.model.*;
+import com.fsd.ap.fx.*;
+
+public class BankApplication extends Application {
+	public static void main(String[] args) { launch(args); }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		ViewLoader.showStage(new Customer("Mr Smith"), "/com/fsd/view/customer.fxml", "Customer", stage);
+	}
+}
---
+++ BankTvFx/src/main/java/com/fsd/ap/fx/Controller.java
@@ -0,0 +1,8 @@
+package com.fsd.ap.fx;
+
+import javafx.stage.*;
+
+public abstract class Controller<M> {
+	protected M model;
+	protected Stage stage;
+}
